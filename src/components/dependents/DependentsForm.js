fd4bebee0b2d4526d236f49f9c028201e022a279
Sololia <getsoli2003@gmail.com>
2020-04-24 15:46:38 +0200

    Cleanup and readme update

--- src/components/dependents/DependentsForm.js
+++ src/components/dependents/DependentsForm.js
@@ -9,7 +9,7 @@ import {
   SYMPTOMS,
   RISKS,
 } from "../../constants/common-keys";
-import DEPENDENTS_ENTRY_FIELDS from "../../constants/dependents-fiels";
+import DEPENDENTS_ENTRY_FIELDS from "../../constants/dependents-fields";
 
 const DependentsForm = ({ onSubmit, lang, langCode, props }) => {
   console.log(langCode);
